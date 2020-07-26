a12d58456c18a3ea17bc7e67e326d2fe25cd9f08
Shaun <shaun@Shauns-MacBook-Pro.local>
2020-07-26 16:37:13 +0800

    deploy to repo

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -9,14 +9,15 @@ const Join = () => {
 
     return(
         <div className="joinOuterContainer">
-            <div className="joinInnerContainer">
+            <div className="joinInnerContainer card">
                 <h1 className="heading">ChatterBox</h1>
-                <div><input placeholder="Name" className="joinInput" name="name" type="text" onChange={(event) => setName(event.target.value)} /></div>
-                <Link onClick={ event => (!name)? event.preventDefault(): null } to={`/chat?name=${name}`}>
-                    <button className="button" type="submit">Enter Chatroom </button>
-                </Link>
+                <div className="form-group">
+                    <input placeholder="Name" className="joinInput form-control" name="name" type="text" onChange={(event) => setName(event.target.value)} />
+                    <Link onClick={ event => (!name)? event.preventDefault(): null } to={`/chat?name=${name}`}>
+                        <button className="btn btn-info joinBtn" type="submit">Enter Chatroom </button>
+                    </Link>
+                </div>
             </div>
-
         </div>
     );
 };
