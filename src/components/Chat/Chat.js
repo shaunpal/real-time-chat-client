c1fe36420d9a4b30db416a2f584366f13f2043e7
shaun <shaun@shauns-MacBook-Air.local>
2023-11-30 22:24:09 +0800

    Updated to vercel serverless

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -20,7 +20,7 @@ const Chat = ({ location }) => {
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
     const [users, setUsers] = useState([]);
-    const ENDPOINT = "https://real-time-chat-server.herokuapp.com/";
+    const ENDPOINT = "https://real-time-chat-server-alpha.vercel.app/";
     const extensions = ["txt", "jpeg", "jpg", "png", "mp3", "mp4", "gif", "pdf"];
     const zoomimg = useSelector(state => state.img);
     const zoomshow = useSelector(state => state.show);
